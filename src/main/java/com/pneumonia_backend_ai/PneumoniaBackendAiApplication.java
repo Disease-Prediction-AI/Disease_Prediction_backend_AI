1f1bab48a676167b766693dcb8193038f70a9587
Yahya Boujrah <yahyaboujrah@Yahyas-MacBook-Air.local>
2023-11-21 17:12:17 +0100

    first commit

---
+++ src/main/java/com/pneumonia_backend_ai/PneumoniaBackendAiApplication.java
@@ -0,0 +1,13 @@
+package com.pneumonia_backend_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PneumoniaBackendAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PneumoniaBackendAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pneumonia_backend_ai/PneumoniaBackendAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pneumonia_backend_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PneumoniaBackendAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
